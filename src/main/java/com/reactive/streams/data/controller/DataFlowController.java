30ee2222bbbb16a01703077bd59912f7e2bf4f71
zaerbib <ilunga.joel@yahoo.fr>
2024-02-09 20:38:41 +0100

    insert one and many document

--- src/main/java/com/reactive/streams/data/controller/DataFlowController.java
+++ src/main/java/com/reactive/streams/data/controller/DataFlowController.java
@@ -1,5 +1,6 @@
 package com.reactive.streams.data.controller;
 
+import com.mongodb.client.result.InsertOneResult;
 import com.reactive.streams.data.service.DataFlowService;
 import org.bson.BsonValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,22 @@ public class DataFlowController {
     }
 
     @GetMapping("/one")
-    public BsonValue generateOne() {
+    public InsertOneResult generateOne() throws Throwable {
         return dataFlowService.generateOne();
+    }
+
+    @GetMapping("/10K")
+    public Integer generate10K() throws Throwable {
+        return dataFlowService.generate10K();
+    }
+
+    @GetMapping("/100K")
+    public Integer generate100K() throws Throwable {
+        return dataFlowService.generate100K();
+    }
+
+    @GetMapping("/1M")
+    public Integer generate1M() throws Throwable {
+        return dataFlowService.generate1M();
     }
 }
