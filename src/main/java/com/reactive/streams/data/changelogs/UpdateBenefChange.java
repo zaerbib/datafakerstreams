ae3f0ca99aa419c573ccc4e21e25c501dae57aa5
zaerbib <ilunga.joel@yahoo.fr>
2024-02-11 00:55:10 +0100

    mongock done

--- src/main/java/com/reactive/streams/data/changelogs/UpdateBenefChange.java
+++ src/main/java/com/reactive/streams/data/changelogs/UpdateBenefChange.java
@@ -3,52 +3,51 @@ package com.reactive.streams.data.changelogs;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.mongodb.client.model.UpdateOneModel;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import com.reactive.streams.data.data.Benef;
-import io.mongock.api.annotations.BeforeExecution;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 import org.bson.Document;
-import org.bson.types.ObjectId;
 
 import java.util.concurrent.Executor;
 
 import static com.reactive.streams.data.utils.DataFlowAsyncChangeLogsUtils.*;
-import static com.reactive.streams.data.utils.JsonPatchUtils.*;
-import static com.mongodb.client.model.Filters.*;
+import static com.reactive.streams.data.utils.JsonPatchUtils.applyJsonPatch;
 
 @ChangeUnit(id = "update-benef-change", order = "001", author = "dev")
+@SuppressWarnings("unused")
 public class UpdateBenefChange {
 
     private final static ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
-    @BeforeExecution
-    private void beforeExcution(MongoCollection<Document> collection,
-                                Executor executor) throws Throwable {
-        // think about it
-    }
 
     @Execution
+    @SuppressWarnings("unused")
     public void changeSet(MongoCollection<Document> collection,
                           Executor executor) throws Throwable {
-        //doDataFlowAsyncChange(collection, executor, this::updateBenefField);
-        doDataFlowUpdateMany(collection,
+        /* when update many */
+        /*doDataFlowUpdateMany(collection,
                 () -> new Document("benef", null),
                 () -> new Document("$set", new Document("benef", benefToDoc(Benef.builder()
                         .invest(0.0)
                         .diff(0.0)
                         .state("normal")
-                        .build()))));
+                        .build()))));*/
+
+        // when update one by one
+        doDataFlowAsyncChangeV2(collection, executor,
+                this::updateBenefFielOneShot,
+                () -> new Document("benef", null));
     }
 
     @RollbackExecution
+    @SuppressWarnings("unused")
     public void rollBackExecution(MongoCollection<Document> collection) {
         // think about it
     }
 
-    private UpdateOneModel<Document> updateBenefField(Document document) {
+    private Document updateBenefFielOneShot(Document document) {
         Double diff = document.getDouble("close") - document.getDouble("open");
         Double invest = document.getDouble("volume") - document.getDouble("dividend");
 
@@ -65,19 +64,18 @@ public class UpdateBenefChange {
                             .diff(diff)
                             .state("up")
                             .build()));
-            return fromDocumentToUpdateOneModel(applyJsonPatch(mapper, document, patchOp));
+            return fromDocumentToDocmentUpdate(applyJsonPatch(mapper, document, patchOp));
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private UpdateOneModel<Document> fromDocumentToUpdateOneModel(Document document) {
-        return new UpdateOneModel<>(eq("uniqueId", document.get("uniqueId")),
-                new Document("$set",
-                        new Document("benef", document.get("benef"))));
-
+    private Document fromDocumentToDocmentUpdate(Document document) {
+        return new Document("$set",
+                        new Document("benef", document.get("benef")));
     }
 
+    @SuppressWarnings("unused")
     private Document benefToDoc(Benef benef) {
         return new Document("invest", benef.getInvest())
                 .append("diff", benef.getDiff())
--- src/main/java/com/reactive/streams/data/utils/DataFlowAsyncChangeLogsUtils.java
+++ src/main/java/com/reactive/streams/data/utils/DataFlowAsyncChangeLogsUtils.java
@@ -2,73 +2,76 @@ package com.reactive.streams.data.utils;
 
 import com.mongodb.bulk.BulkWriteResult;
 import com.mongodb.client.model.UpdateManyModel;
-import com.mongodb.client.model.UpdateOneModel;
 import com.mongodb.client.result.UpdateResult;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 import java.util.function.Supplier;
-
-import static com.reactive.streams.data.utils.DataFlowGenerate.paritionList;
-import static com.reactive.streams.data.utils.SubscriberHelpers.ListObservableSubscriber;
-import static com.reactive.streams.data.utils.SubscriberHelpers.ObservableSubscriber;
+import java.util.function.UnaryOperator;
 
 
 @Slf4j
 @UtilityClass
 public class DataFlowAsyncChangeLogsUtils {
 
-    private static final ListObservableSubscriber<Document> listSubscriber
-            = new ListObservableSubscriber<Document>();
-    private static final ObservableSubscriber<BulkWriteResult> observaleSubscriber
-            = new ObservableSubscriber<>();
-    private static final ObservableSubscriber<UpdateResult> observaleSubscriberUpdateMany
-            = new ObservableSubscriber<>();
-    public void doDataFlowAsyncChange(MongoCollection<Document> collection,
-                                      Executor executor,
-                                      Function<Document, UpdateOneModel<Document>> update) throws Throwable {
-        changeUnitAsyncVersion(collection, executor, update);
+    private static final SubscriberHelpers.ListObservableSubscriber<Document> listSubscriber
+            = new SubscriberHelpers.ListObservableSubscriber<>();
+    private static final SubscriberHelpers.ObservableSubscriber<BulkWriteResult> observaleSubscriber
+            = new SubscriberHelpers.ObservableSubscriber<>();
+    private static final SubscriberHelpers.ObservableSubscriber<UpdateResult> observaleSubscriberUpdateMany
+            = new SubscriberHelpers.ObservableSubscriber<>();
+
+    public void doDataFlowAsyncChangeV2(MongoCollection<Document> collection,
+                                        Executor executor,
+                                        UnaryOperator<Document> update,
+                                        Supplier<Document> filter) throws Throwable {
+        changeUnitAsyncVersion2(collection, executor, update, filter);
     }
 
+    @SuppressWarnings("unused")
     public void doDataFlowUpdateMany(MongoCollection<Document> collection,
                                      Supplier<Document> filterField,
                                      Supplier<Document> update) throws Throwable {
-         changeUnitUpdateMany(collection, filterField, update);
+        changeUnitUpdateMany(collection, filterField, update);
     }
 
-    private void changeUnitAsyncVersion(MongoCollection<Document> collection,
-                                       Executor executor,
-                                       Function<Document, UpdateOneModel<Document>> update) throws Throwable {
+    private void changeUnitAsyncVersion2(MongoCollection<Document> collection,
+                                         @SuppressWarnings("unused") Executor executor,
+                                         UnaryOperator<Document> update,
+                                         Supplier<Document> filter) throws Throwable {
         collection.find(new Document()).subscribe(listSubscriber);
 
-        List<UpdateOneModel<Document>> received = listSubscriber.get(300, TimeUnit.SECONDS)
+        List<Document> received = listSubscriber.get(300, TimeUnit.SECONDS)
                 .stream()
                 .filter(Objects::nonNull)
                 .map(update)
                 .toList();
 
-        paritionList(received, 1000)
+        DataFlowGenerate.paritionList(received, 1000)
                 .forEach(item -> {
-                    collection.bulkWrite(item).subscribe(observaleSubscriber);
+                    collection.bulkWrite(Collections.singletonList(new UpdateManyModel<>(filter.get(), item)))
+                            .subscribe(observaleSubscriber);
                     try {
-                        log.info("Updated size : " + observaleSubscriber.get(300, TimeUnit.SECONDS).size());
+                        observaleSubscriber.get(600, TimeUnit.SECONDS);
                     } catch (Throwable e) {
                         throw new RuntimeException(e);
                     }
                 });
+
+        log.info("Mongock competed !!!");
     }
 
     private void changeUnitUpdateMany(MongoCollection<Document> collection,
-                                     Supplier<Document> filterField,
-                                     Supplier<Document> update) throws Throwable {
+                                      Supplier<Document> filterField,
+                                      Supplier<Document> update) throws Throwable {
         collection.updateMany(filterField.get(), update.get()).subscribe(observaleSubscriberUpdateMany);
-        log.info("Update result : " +observaleSubscriberUpdateMany.get(300, TimeUnit.SECONDS));
+        log.info("Update result : " + observaleSubscriberUpdateMany.get(300, TimeUnit.SECONDS));
     }
 }
